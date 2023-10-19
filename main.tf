816782e4acdbcffa78ad4328665b8ef03ea7dda5
Bruno Walker <bwmacedo@hotmail.com>
2023-10-19 14:46:55 -0300

    token

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "github.com/bwalkermacedo/modulos-terraform"
+  source = "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@github.com/bwalkermacedo/modulos-terraform"
 
   name = var.name_vpc
   cidr = var.cidr
